ad82d9829214f71c461f200a45f2a1bb10d1b078
Neosoulink <neodarksoulink@gmail.com>
2022-02-18 06:24:42 +0100

    add module resolver

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -8,6 +8,10 @@ const resolvers = {
 		track: (_, { id }, { dataSources }) => {
 			return dataSources.trackAPI.getTrack(id);
 		},
+		// get a single module by ID, for the module detail page
+		module: (_, { id }, { dataSources }) => {
+			return dataSources.trackAPI.getModule(id);
+		},
 	},
 	Track: {
 		author: ({ authorId }, _, { dataSources }) => {
